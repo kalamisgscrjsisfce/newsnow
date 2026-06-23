dc2d64e12879f2e5d5d6b6822e49298c5f81d460
kalamisgscrjsisfce <kalamisgscrjsisfce@gmail.com>
2026-06-23 13:17:00 +0800

    Update footer link to point to Homepage

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <>
-      <a href={`${Homepage}/blob/main/LICENSE`} target="_blank">支持一下，请投币</a>
+      <a href={`${Homepage}`} target="_blank">支持一下，请投币</a>
       <span>
         <span>NewsNow © 1998 By </span>
         <a href={Author.url} target="_blank">
