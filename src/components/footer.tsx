0d5f572da3a8c88cdf2b2814e64e7c7da4ef47b5
kalamisgscrjsisfce <kalamisgscrjsisfce@gmail.com>
2026-06-23 13:09:42 +0800

    Update footer text and year in Footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,9 @@
 export function Footer() {
   return (
     <>
-      <a href={`${Homepage}/blob/main/LICENSE`} target="_blank">MIT LICENSE</a>
+      <a href={`${Homepage}/blob/main/LICENSE`} target="_blank">友链</a>
       <span>
-        <span>NewsNow © 2024 By </span>
+        <span>NewsNow © 1998 By </span>
         <a href={Author.url} target="_blank">
           {Author.name}
         </a>
